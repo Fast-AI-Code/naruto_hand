0b2c43407ca427553f377f67b6577da165c5d957
SubhadityaMukherjee <msubhaditya@gmail.com>
2020-03-03 12:05:57 +0530

    extended to video

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 - Naruto Hand sign recognition
 - Dataset : https://www.kaggle.com/vikranthkanumuru/naruto-hand-sign-dataset
 - Please look at the jupyter notebook 
-- ![naruto](img2.png)
-
+- Look at output.mkv for an example
 - ![naruto](img1.png)
+-  ![naruto](img2.png)
--- runner.py
+++ runner.py
@@ -1,5 +1,5 @@
 from fastai.vision import *
-import cv2 
+import cv2
 from PIL import Image as Img
 import os
 import matplotlib.pyplot as plt
@@ -7,9 +7,34 @@ import matplotlib.pyplot as plt
 
 learn = load_learner('model')
 learn.load('fin')
-for a in os.listdir('test/'):
-	if '.png' in a:
-		img = open_image(f'test/{a}')
-		print(learn.predict(img)[0])
-		
-		cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+# for a in os.listdir('test/'):
+# 	if '.png' in a:
+# 		img = open_image(f'test/{a}')
+# 		# print(learn.predict(img)[0])
+
+# 		cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+vid = cv2.VideoCapture('test/an.mp4')
+frame_width = int(vid.get(3))
+frame_height = int(vid.get(4))
+out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc(
+    'M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+
+while(vid.isOpened()):
+    ret, frame = vid.read()
+    # try:
+    t = torch.tensor(np.ascontiguousarray(
+        np.flip(frame, 2)).transpose(2, 0, 1)).float() / 255
+    img = Image(t)
+    p = learn.predict(img)[0]
+    print(p)
+
+    cv2.putText(frame, str(p), (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 0, 255), 2, cv2.LINE_AA)
+    out.write(frame)
+
+    # except Exception as e:
+    # 	print(e)
+    #     break
+
+out.release()
+vid.release()
